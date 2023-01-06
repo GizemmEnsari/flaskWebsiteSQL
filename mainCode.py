7fed2a7eb755a403ad681b360a1fc93505417f76
gizemmensari <gensari@mun.ca>
2023-01-06 03:12:14 -0330

    update

--- mainCode.py
+++ mainCode.py
@@ -15,11 +15,10 @@ def checkCredentials():
             return render_template("login.html",note="Error, No Entry")
         else:
             usr = User.query.filter_by(username = inptUsrnm).first()
-            if usr == None:
+            if usr == None: # user credentials not in database
                 return render_template("login.html", note = "error, username not found")
             else:
                 if inptPswrd == usr.password:
-                  #  print("giz")
                     return render_template("index.html", username = inptUsrnm ,note = "logged in as "+ inptUsrnm)
                 else:
                     return render_template("index.html" , note = "Error, wrong password or username ")
@@ -28,7 +27,6 @@ def checkCredentials():
 def createUser():
 
     if request.method == "POST":
-    #    print("post")
         inptUsrnm = request.form["username"]
         inptPswrd = request.form["password"]
     if inptUsrnm == "" or inptPswrd == "":
@@ -36,7 +34,7 @@ def createUser():
 
     if inptUsrnm != "" and inptPswrd != "":
         usr = User.query.filter_by(username=inptUsrnm).first()
-        if usr != None:
+        if usr != None:  # if user already exists
             return render_template("login.html", message = "Error, this user already exists")
         user = User(
             username = inptUsrnm,
@@ -47,28 +45,48 @@ def createUser():
         server_db.session.add(user)
         server_db.session.commit()
         print(user)
-#         print("KKJ")
-        return render_template("index.html", username = inptUsrnm)
-   # print("retttt")
-    return render_template("index.html", username = inptUsrnm)
-# print("fssfs")
+        return render_template("index.html", note = "account created as "+ inptUsrnm)
+    return render_template("index.html", note = "account created as "+ inptUsrnm)
 
 
 @app.route("/logout", methods=["POST","GET"] )
 def logout():
     return render_template("login.html")
 
+@app.route("/runCode", methods=['POST', 'GET'])
+def runCode():
+    if request.method == 'POST':
+        code = request.form["enterCode"]
+        p = run("python", stdout=PIPE, shell=True, stderr=STDOUT, input=code, encoding='ascii')
+        output = p.stdout
+        return render_template("index.html", print_output=output, codearea=code)
+    else:
+        return render_template("index.html")
+
 @app.route("/enterCode", methods=["POST","GET"])
 def enterCode():
     pass
 
+@app.route("/mySpace1" , methods = ["POST", "GET"])
+def mySpace1():
+    return render_template("space1.html" )
+
+
+@app.route("/mySpace2", methods = ["POST","GET"])
+def mySpace2():
+    return render_template("space2.html")
+
+@app.route("/mySpace3",methods = ["POST", "GET"])
+def mySpace3():
+    return render_template("space3.html")
+
 
 @app.route("/myProfile",methods=["POST", "GET"])
 def myProfile():
-    global usr
+    #global usr
     if request.method == "POST":
-        inptUsrnm = User.query.filter_by(username = )
-        return render_template("myProfile.html", inptUsrnm)
+        return render_template("myProfile.html", inptUsrnm = usr.username)
+    pass
 
 def main():
     with app.app_context():
